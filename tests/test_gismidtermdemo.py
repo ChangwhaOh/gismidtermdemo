cc583e6a85e8d4f4f0abde0c0d75010c13e817e8
ChangwhaOh <coh4@vols.utk.edu>
2023-03-23 14:50:00 -0600

    set a new repository

---
+++ tests/test_gismidtermdemo.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+"""Tests for `gismidtermdemo` package."""
+
+
+import unittest
+
+from gismidtermdemo import gismidtermdemo
+
+
+class TestGismidtermdemo(unittest.TestCase):
+    """Tests for `gismidtermdemo` package."""
+
+    def setUp(self):
+        """Set up test fixtures, if any."""
+
+    def tearDown(self):
+        """Tear down test fixtures, if any."""
+
+    def test_000_something(self):
+        """Test something."""
